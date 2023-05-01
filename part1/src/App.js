39005c01385afcdd9bfa4d1d6d5411ef1e29988c
LuigiMagdamitUCLA <luigiantonmagdamit@gmail.com>
2023-05-01 12:59:58 -0700

    finished 1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -20,11 +20,12 @@ const Part = (props) => {
   )
 }
 const Content = (props) => {
+  console.log(props)
   return (
     <div>
-      <Part />
-      <Part />
-      <Part />
+      <Part part = {props.parts[0].name} exercises = {props.parts[0].exercises}/>
+      <Part part = {props.parts[1].name} exercises = {props.parts[1].exercises}/>
+      <Part part = {props.parts[2].name} exercises = {props.parts[2].exercises}/>
     </div>
   )
 }
@@ -38,19 +39,25 @@ const Total = (props) => {
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
 
   return (
     <div>
       <Header course = "butt"/>
-      <Content />
-      <Total total = {exercises1 + exercises2 + exercises3}/>
+      <Content parts = {[part1, part2, part3]}/>
+      <Total total = {part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
